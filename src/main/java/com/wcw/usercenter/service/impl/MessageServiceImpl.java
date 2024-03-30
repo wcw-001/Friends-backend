dd84e0a392939890872790d2a1dbb11015c77681
wcw-001 <wcw2314075528@gmail.com>
2024-03-30 13:29:08 +0800

    add 添加聊天模块 update 优化界面

---
+++ src/main/java/com/wcw/usercenter/service/impl/MessageServiceImpl.java
@@ -0,0 +1,126 @@
+package com.wcw.usercenter.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wcw.usercenter.common.ErrorCode;
+import com.wcw.usercenter.exception.BusinessException;
+import com.wcw.usercenter.mapper.MessageMapper;
+import com.wcw.usercenter.model.domain.Message;
+import com.wcw.usercenter.model.domain.User;
+import com.wcw.usercenter.model.vo.MessageVO;
+import com.wcw.usercenter.model.vo.UserVo;
+import com.wcw.usercenter.service.MessageService;
+import com.wcw.usercenter.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.wcw.usercenter.contant.RedisConstants.MESSAGE_BLOG_NUM_KEY;
+import static com.wcw.usercenter.contant.RedisConstants.MESSAGE_LIKE_NUM_KEY;
+
+
+/**
+ * @author wcw
+ * @description 针对表【message】的数据库操作Service实现
+ * @createDate 2023-06-21 17:39:30
+ */
+@Service
+public class MessageServiceImpl extends ServiceImpl<MessageMapper, Message>
+        implements MessageService {
+    @Resource
+    @Lazy
+    private UserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public long getMessageNum(Long userId) {
+        LambdaQueryWrapper<Message> messageLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        messageLambdaQueryWrapper.eq(Message::getToId, userId).eq(Message::getIsRead, 0);
+        return this.count(messageLambdaQueryWrapper);
+    }
+
+    @Override
+    public long getLikeNum(Long userId) {
+        String likeNumKey = MESSAGE_LIKE_NUM_KEY + userId;
+        Boolean hasLike = stringRedisTemplate.hasKey(likeNumKey);
+        if (Boolean.TRUE.equals(hasLike)) {
+            String likeNum = stringRedisTemplate.opsForValue().get(likeNumKey);
+            assert likeNum != null;
+            return Long.parseLong(likeNum);
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public List<MessageVO> getLike(Long userId) {
+        LambdaQueryWrapper<Message> messageLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        messageLambdaQueryWrapper.eq(Message::getToId, userId)
+                .and(wp -> wp.eq(Message::getType, 0).or().eq(Message::getType, 1))
+                .orderBy(true, false, Message::getCreateTime);
+        List<Message> messageList = this.list(messageLambdaQueryWrapper);
+        if (messageList.isEmpty()) {
+            return new ArrayList<>();
+        }
+        LambdaUpdateWrapper<Message> messageLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        messageLambdaUpdateWrapper.eq(Message::getToId, userId).eq(Message::getType, 0)
+                .or().eq(Message::getType, 1).set(Message::getIsRead, 1);
+        this.update(messageLambdaUpdateWrapper);
+        String likeNumKey = MESSAGE_LIKE_NUM_KEY + userId;
+        Boolean hasLike = stringRedisTemplate.hasKey(likeNumKey);
+        if (Boolean.TRUE.equals(hasLike)) {
+            stringRedisTemplate.opsForValue().set(likeNumKey, "0");
+        }
+        return messageList.stream().map((item) -> {
+            MessageVO messageVO = new MessageVO();
+            BeanUtils.copyProperties(item, messageVO);
+            User user = userService.getById(messageVO.getFromId());
+            if (user == null) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "发送人不存在");
+            }
+            UserVo userVO = new UserVo();
+            BeanUtils.copyProperties(user, userVO);
+            messageVO.setFromUser(userVO);
+            return messageVO;
+        }).collect(Collectors.toList());
+    }
+
+
+    @Override
+    public Boolean hasNewMessage(Long userId) {
+        String likeNumKey = MESSAGE_LIKE_NUM_KEY + userId;
+        Boolean hasLike = stringRedisTemplate.hasKey(likeNumKey);
+        if (Boolean.TRUE.equals(hasLike)) {
+            String likeNum = stringRedisTemplate.opsForValue().get(likeNumKey);
+            assert likeNum != null;
+            if (Long.parseLong(likeNum) > 0) {
+                return true;
+            }
+        }
+        String blogNumKey = MESSAGE_BLOG_NUM_KEY + userId;
+        Boolean hasBlog = stringRedisTemplate.hasKey(blogNumKey);
+        if (Boolean.TRUE.equals(hasBlog)) {
+            String blogNum = stringRedisTemplate.opsForValue().get(blogNumKey);
+            assert blogNum != null;
+            return Long.parseLong(blogNum) > 0;
+        }
+        return false;
+    }
+}
+
+
+
+
